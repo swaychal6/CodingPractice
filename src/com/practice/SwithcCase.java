42b7c21ced33243918f8de9348671a774f97bead
Shubham Waychal <swaychal6@gmail.com>
2024-02-03 14:53:10 +0530

    new code added

--- src/com/practice/SwithcCase.java
+++ src/com/practice/SwithcCase.java
@@ -3,23 +3,5 @@ package com.practice;
 import java.util.Scanner;
 
 public class SwithcCase {
-public static void main(String[] args) {
-	
-	Scanner sc=new Scanner(System.in);
-	int nextInt = sc.nextInt();
-	
-	switch (nextInt) {
-	case 1: 
-		
-		System.out.println("Monday");
-	
-	case 2:
-		System.out.println("ravivar");
-	
-	default:
-		System.out.println("default run");
-	
-}
-
-}
+public static void main(String[] args) {}
 }
